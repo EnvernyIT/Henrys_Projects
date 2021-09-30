1f181a9c89735cd95a64cef76ce79252b4e5b6b5
Enverny Iengibe <envernyiengibe@gmail.com>
2021-09-29 21:03:11 -0300

    Kung Lao Added. Still working out the kinks

--- src/com/bludots/assignments/MortalKombat/KungLao.java
+++ src/com/bludots/assignments/MortalKombat/KungLao.java
@@ -4,20 +4,19 @@ public class KungLao extends BasicFighter implements Supers{
 
 	@Override
 	public void Attack(int health) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Hat Throw attack");
+		health = health - 10;		
 	}
 
 	@Override
 	public void specialAttack(int health) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Whirlwind Spin Attack");
+		health = health - 20;
 	}
 
 	@Override
 	public void Fatality() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Throws the hat pass the opponent and calls it back and slices its head off. Back to Kung Lao!");		
 	}
 
 }
